71c180ffa3426aa2b1940503becbe3d2115b020b
Jiuyang liu <liujiuyang1994@gmail.com>
2018-10-21 13:52:45 +0800

    update chnroutes to static route.

--- chnroutes2.py
+++ chnroutes2.py
@@ -4,10 +4,9 @@ import urllib.request
 
 with open(sys.argv[1], 'r+') as r:
     data = json.load(r)
-    data["firewall"]["group"]["address-group"]["chnroutes2"]["address"] = []
     for line in urllib.request.urlopen("https://raw.githubusercontent.com/ym/chnroutes2/master/chnroutes.txt"):
         line = line.decode('ascii')[:-1]
         if not line.startswith("#"):
-            data["firewall"]["group"]["address-group"]["chnroutes2"]["address"].append(line)
-    with open(sys.argv[2], 'w') as w: 
+            data["protocols"]["static"]["interface-route"][line] = {"next-hop-interface": {"pppoe0": {"distance": 1}}}
+    with open(sys.argv[2], 'w') as w:
         json.dump(data, w)
--- chnroutes2.sh
+++ chnroutes2.sh
@@ -2,22 +2,13 @@
 vop=/opt/vyatta/bin/vyatta-op-cmd-wrapper
 vcfg=/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper
 
-internal_chnroutes2_list=$($vop show firewall group chnroutes2 | grep -A5 Members |grep -v Members | awk '{ print $1 }' )
-
 #get chnroutes2 from ym/chnroutes2
 external_chnroutes2_ip_list=$(curl https://raw.githubusercontent.com/ym/chnroutes2/master/chnroutes.txt | grep -v ^# |awk '{print $1}')
 
-if [ "$internal_chnroutes2_list" = "$external_chnroutes2_ip_list" ]; then
-:
-else
-#if addresses have changed, remove address-group "chnroutes2" and recreate it with the new addresses
-    $vcfg begin
-    $vcfg delete firewall group address-group chnroutes2
-    $vcfg set firewall group address-group chnroutes2 description "chnroutes2 IPs"
-    for ip in ${external_chnroutes2_ip_list}; do
-        $vcfg set firewall group address-group chnroutes2 address "$ip"
-    done
-    $vcfg commit
-    $vcfg save
-    $vcfg end
-fi
+$vcfg begin
+for ip in ${external_chnroutes2_ip_list}; do
+    $vcfg set protocols static interface-route ${ip} next-hop-interface pppoe0 distance 1
+done
+$vcfg commit
+$vcfg save
+$vcfg end
